0c71434b81d6167817c65b2bbada63339885fbf8
wilkor <wilkorguitarrista@msn.com>
2018-11-29 09:24:29 -0200

    retirando url que estava comentada

--- consulta.js
+++ consulta.js
@@ -3,10 +3,9 @@ var http = require('http');
 module.exports = function (callback, cep) {
 
   let p = new Promise((resolve, reject) => {
-    
+
     const config = {
       url: `https://viacep.com.br/ws/${removeCaracteresEspeciaisDoCep(cep)}/json`,
-      //'https://santander.custhelp.com/cc/MontaView/searchTalk?id=6527964&key=Y2hhdHNhcmFzYW50YW5kZXI=',
       method: 'GET'
     };
     request(config, (error, response, body) => {
